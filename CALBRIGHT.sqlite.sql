8da17fd39271218f44d3a5c967257c67dca7bcf2
hilalmgt <127870931+hilalmgt@users.noreply.github.com>
2024-07-30 02:06:23 -0700

    Add files via upload

---
+++ CALBRIGHT.sqlite.sql
@@ -0,0 +1,29 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE "people" (
+	"id_number"	INTEGER NOT NULL UNIQUE,
+	"first_name"	TEXT,
+	"last_name"	TEXT,
+	"city"	TEXT,
+	"state_code"	TEXT,
+	"shirt_or_hat"	TEXT,
+	"quiz_points"	INTEGER,
+	"team"	TEXT,
+	"signup"	TEXT,
+	"age"	INTEGER,
+	"company"	TEXT,
+	PRIMARY KEY("id_number" AUTOINCREMENT)
+);
+CREATE TABLE "states" (
+	"state_name"	TEXT,
+	"state_abbrev"	TEXT,
+	"region"	TEXT,
+	"division"	TEXT
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
